d8b7a439d7afc50797e2d9028cf9fb0803028e71
root <timsidie@gmail.com>
2014-12-08 05:37:31 +0000

    Link in footer, score in account page.

--- account.php
+++ account.php
@@ -13,6 +13,26 @@ if ($_SESSION['name']) {
 <div class="row">
 <h1 style="color: white; margin-left: 12%;"><?php echo $name; ?></h1>
 <br><br><img style="margin-left: 12%;" src=<?php echo "./avatars/".$name.".jpg" ?>>
+<br><br>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+ 
+$conn = mysql_connect($servername, $username, $password)
+or die("\nUnable to connect to MySQL.\n");
+ 
+$selected = mysql_select_db("codeagon", $conn)
+or die("\nCould not select db.\n");
+
+$query = "SELECT score FROM tbl_users WHERE BINARY username='".$name."'";
+$result = mysql_query($query);
+$row = mysql_fetch_row($result);
+$score = $row[0];
+
+echo "<h4 style='margin-left:12%; color: white;'>Your score so far: ".$score."</h4>"; 
+?>
 </div>
 </div>
 <?php include './footer.html' ?>
