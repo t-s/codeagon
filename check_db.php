68942a7da4e703c67da80e5262c3d82e17bb1bef
Tim Sidie <timsidie@gmail.com>
2014-12-17 00:19:24 -0500

    Check db

---
+++ check_db.php
@@ -0,0 +1,11 @@
+<?php
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    echo $url;
+    
+
+?>
